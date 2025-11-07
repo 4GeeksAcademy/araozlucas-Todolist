860f8f3609425a3270ae44ea8fef556daec833fd
Pimple <araozlucas@gmail.com>
2025-11-07 20:14:02 +0000

    Second commit

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 export const Todolist = () => {
     
-    const handleEmail = (event) => {setEmail(event.target.value)};
+    const toDos = []; 
+
+    const [task, setTask] = useState ('');
+    const handleTask = (event) => {setTask(event.target.value)};
+    const handleSubmit = (event) => {
+    // Tienes que agregar la tarea al arra de Todos    
+        event.preventDefault()
+        toDos.push(task)
+        console.log(toDos);
+        
+    };
 
     return (
         <div className="container text-start">
             <h1 className="text-center mt-10 quicksand-light text-danger">todos</h1>
             <div className="col-10 col-sm-8 col-md-6 col-lg-4 m-auto">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="exampleInputTask" className="form-label"></label>
-                        <input type="text" className="form-control" id="exampleTask" aria-describedby="emailHelp" placeholder="New task"/>
+                        <input type="text" className="form-control" id="exampleTask" aria-describedby="emailHelp" placeholder="New task" 
+                        value={task} onChange={handleTask}/>
                     </div>
                 </form>
             </div>
