1128408dd167aeca2d37301e210cc297006d2d3b
Pimple <araozlucas@gmail.com>
2025-11-07 20:34:58 +0000

    Third commit

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -6,10 +6,12 @@ export const Todolist = () => {
     const toDos = []; 
 
     const [task, setTask] = useState ('');
+    
     const handleTask = (event) => {setTask(event.target.value)};
     const handleSubmit = (event) => {
     // Tienes que agregar la tarea al arra de Todos    
         event.preventDefault()
+        setTask('');
         toDos.push(task)
         console.log(toDos);
         
@@ -20,10 +22,14 @@ export const Todolist = () => {
             <h1 className="text-center mt-10 quicksand-light text-danger">todos</h1>
             <div className="col-10 col-sm-8 col-md-6 col-lg-4 m-auto">
                 <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputTask" className="form-label"></label>
-                        <input type="text" className="form-control" id="exampleTask" aria-describedby="emailHelp" placeholder="New task" 
-                        value={task} onChange={handleTask}/>
+                    <div className="container">
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <label htmlFor="exampleInputTask" className="form-label"></label>
+                                <input type="text" className="form-control" id="exampleTask" aria-describedby="emailHelp" placeholder="New task" 
+                                value={task} onChange={handleTask}/>
+                            </li>
+                        </ul>
                     </div>
                 </form>
             </div>
